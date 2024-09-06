94284ba3742594282f16be8d22caa8cbca986dd8
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-06 07:37:35 +0700

    style: format code using Prettier

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,20 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Ads Management",
+  title: 'Ads Management',
   description:
-    "The Ads Management System is a comprehensive platform designed to streamline the management of advertising campaigns, offering a user-friendly interface for selecting specific dates, times, and locations for ad displays on designated monitors.",
-};
+    'The Ads Management System is a comprehensive platform designed to streamline the management of advertising campaigns, offering a user-friendly interface for selecting specific dates, times, and locations for ad displays on designated monitors.',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} antialiased>{children}</body>
+      <body className={inter.className} antialiased>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
