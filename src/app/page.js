348d851a349d5d64b73cf48fb199f5dbe50c7945
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-08 15:00:20 +0700

    feat: create `MonitorCard` component to display monitor details

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import MonitorCard from '@/app/ui/monitor-card'
+
 export default function Home() {
-  return <main className=""></main>
+  return (
+    <main className="max-w-5xl mx-auto px-4">
+        <MonitorCard />
+    </main>
+  )
 }
