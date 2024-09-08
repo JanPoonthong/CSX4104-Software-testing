7d65d945276da9ad2682f9268262db67ee86eebd
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-08 15:04:43 +0700

    style: format code using Prettier

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import MonitorCard from '@/app/ui/monitor-card'
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto px-4">
-        <MonitorCard />
+      <MonitorCard />
     </main>
   )
 }
