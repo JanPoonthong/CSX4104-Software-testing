ecc45d45221e5e803acaa03de46c420793a67911
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-10 10:29:36 +0700

    feat: display `DatePicker` and `ProvinceSelector` component on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 import MonitorCard from '@/app/ui/monitor-card'
+import ProvinceSelector from './ui/province-selector'
+import DatePicker from './ui/data-picker'
 
 export default function Home() {
   return (
-    <main className="max-w-5xl mx-auto px-4">
+    <main className="max-w-5xl mx-auto my-5 px-4">
+      <div className="flex gap-5 my-3 items-center">
+        <ProvinceSelector />
+        <DatePicker labelName={'Start date'} />
+        <DatePicker labelName={'End date'} />
+      </div>
       <MonitorCard />
     </main>
   )
