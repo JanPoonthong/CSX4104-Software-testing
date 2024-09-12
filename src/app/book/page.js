ad9ef9f28881bbcaf850409c35676376e2ae7ba9
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-12 10:18:25 +0700

    feat: add monitor booking form

---
+++ src/app/book/page.js
@@ -0,0 +1,9 @@
+import MonitorBook from '../ui/monitor-book'
+
+export default function Home() {
+  return (
+    <main className="max-w-5xl mx-auto my-5 px-4 flex justify-center">
+      <MonitorBook />
+    </main>
+  )
+}
