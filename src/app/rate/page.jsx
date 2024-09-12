da6148513ff9e3c98c538917c08c13c8c3a7050a
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-12 10:59:09 +0700

    feat: add rate calculation

---
+++ src/app/rate/page.jsx
@@ -0,0 +1,9 @@
+import RateCalculation from '../ui/rate-calculation'
+
+export default function Home() {
+  return (
+    <main className="max-w-5xl mx-auto my-5 px-4 flex justify-center">
+      <RateCalculation />
+    </main>
+  )
+}
---
+++ src/app/ui/rate-calculation.jsx
@@ -0,0 +1,17 @@
+export default function RateCalculation() {
+  return (
+    <div className="w-1/2 p-4 border border-gray-200">
+      <form className="space-y-6">
+        <h5 className="text-xl font-medium text-gray-900">
+          Rate calculation
+        </h5>
+        <button
+          type="submit"
+          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Done
+        </button>
+      </form>
+    </div>
+  )
+}
